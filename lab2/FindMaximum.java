f8e2d330764179a55ba20df523dba9c9ae912ae9
U220709065 <cerendoganay27@gmail.com>
2024-02-24 20:15:26 +0300

    lab2

--- lab2/FindMaximum.java
+++ lab2/FindMaximum.java
@@ -1,15 +1,21 @@
 public class FindMaximum {
 
-	public static void main(String[] args){
-		int value1 = 1;
-        int value2 = 2;
-        int result;
+    public static void main(String[] args){
 
-        boolean someCondition = true;
+        if(args.length > 0){
 
-        result = someCondition ? value1 : value2;
+            int value1 = Integer.parseInt(args[0]);
+            int value2 = Integer.parseInt(args[1]);
 
-        System.out.println(result);
+            //int value1 = 1;
+            //int value2 = 4;
+            int result;
 
-	}
+            boolean someCondition = value1 > value2;
+            result = someCondition ? value1 : value2;
+
+            System.out.println(result);
+
+        }
+    }
 }
