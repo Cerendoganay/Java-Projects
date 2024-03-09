9234ced5a05ca705bd3042c50a18e868dccd0799
U220709065 <cerendoganay27@gmail.com>
2024-03-09 23:18:19 +0300

    lab4

--- lab4/TicTacToe.java
+++ lab4/TicTacToe.java
@@ -5,26 +5,49 @@ public class TicTacToe {
 
 	public static void main(String[] args) throws IOException {
 		Scanner reader = new Scanner(System.in);
-		char[][] board = { { ' ', ' ', ' ' }, { ' ', ' ', ' ' }, { ' ', ' ', ' ' } };
+		char[][] board = {{' ', ' ', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '}};
+
 
 		printBoard(board);
 
-		System.out.print("Player 1 enter row number:");
-		int row = reader.nextInt();
-		System.out.print("Player 1 enter column number:");
-		int col = reader.nextInt();
-		board[row - 1][col - 1] = 'X';
-		printBoard(board);
+		int movecnt = 0;
+		int currentplayer = 0;
+		while (movecnt < 9) {
+			System.out.print("Player " + (currentplayer + 1) + "enter row number:");
+			int row = reader.nextInt();
+			System.out.print("Player " + (currentplayer + 1) + "enter column number:");
+			int col = reader.nextInt();
+			while (((row < 1 || col < 1) || (row > 3 || col > 3)) || (board[row - 1][col - 1] != ' ')) {
+				System.out.println("Invalid move! Please choose another place");
+				System.out.print("Player " + (currentplayer + 1) + "enter row number:");
+				row = reader.nextInt();
+				System.out.print("Player " + (currentplayer + 1) + "enter column number:");
+				col = reader.nextInt();
+			}
+			if (currentplayer == 0) {
+				board[row - 1][col - 1] = 'X';
+			} else {
+				board[row - 1][col - 1] = '0';
+			}
+			printBoard(board);
 
-		System.out.print("Player 2 enter row number:");
-		row = reader.nextInt();
-		System.out.print("Player 2 enter column number:");
-		col = reader.nextInt();
-		board[row - 1][col - 1] = 'O';
-		printBoard(board);
+			if(checkBoard(board)){
+				System.out.println("Player "+(currentplayer+1) +"won the game");
+				break;
+			}
 
+			currentplayer = (currentplayer + 1) % 2;
+			movecnt++;
+
+			if(movecnt==9){
+				System.out.println("Its a draw!");
+
+			}
+
+		}
 		reader.close();
 	}
+
 
 	public static void printBoard(char[][] board) {
 		System.out.println("    1   2   3");
@@ -42,6 +65,28 @@ public class TicTacToe {
 			System.out.println("   -----------");
 
 		}
+
+	}
+
+	public static boolean checkBoard(char[][] board) {
+		// Check all the row
+		for (int row = 0; row < 3; row++) {
+			if ((board[row][0] == board[row][1]) && (board[row][1] == board[row][2]) && board[row][0] != ' ')
+				return true;
+		}
+		// Check all the columns
+		for (int col = 0; col < 3; col++) {
+			if ((board[0][col] == board[1][col]) && (board[1][col] == board[2][col]) && board[0][col] != ' ')
+				return true;
+		}
+		// Check all the top-left to bottom-right
+		if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != ' ')
+			return true;
+		// Check all the bottom-left to top-right
+		if (board[2][0] == board[1][1] && board[1][1] == board[0][2] && board[2][0] != ' ')
+			return true;
+		// No winner yet
+		return false;
 
 	}
 
